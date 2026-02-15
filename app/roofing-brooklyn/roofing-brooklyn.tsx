116608ce6f3b80e8974f99b58f7773f98d155dc8
verizon <sfierce305@gmail.com>
2026-02-15 22:37:48 +0300

    Merged changes

--- app/roofing-brooklyn/roofing-brooklyn.tsx
+++ app/roofing-brooklyn/roofing-brooklyn.tsx
@@ -1,9 +1,13 @@
-"use client";
+// app/roofing-brooklyn/page.tsx
+import React from 'react';
 
-export default function RoofingBrooklyn2() {
+const RoofingBrooklynPage = () => {
   return (
     <div>
       <h1>Roofing Brooklyn</h1>
+      {/* Add your content here */}
     </div>
   );
-}
+};
+
+export default RoofingBrooklynPage;
